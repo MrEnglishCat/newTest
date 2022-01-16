459e016c00e931d429a025fdb274e1db4b195cd2
Serge Antonenko <93328309+MrEnglishCat@users.noreply.github.com>
2022-01-16 18:46:22 +0000

    three arrays

--- main.cs
+++ main.cs
@@ -6,7 +6,20 @@ public class Program
 {
   public static void Main()
   {
-   
+    Console.WriteLine("asda");
+    int[] id = new int [10];
+    string[] names = new string[10] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+    string[] books = new string[10] {"A_book", "B_book", "C_book", "D_book", "E_book", "F_book", "G_book", "H_book", "I_book", "J_book"};
+
+    for (int i = 0; i < 10; i++)
+    {
+        id[i] = i;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"id: {id[i]}\t Name: {names[i]}\t Book: {books[i]}");
+    }
   }
 
 }

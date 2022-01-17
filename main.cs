d22df844b00318706cca96b631314a187d95e4fd
Serge Antonenko <93328309+MrEnglishCat@users.noreply.github.com>
2022-01-17 18:48:43 +0000

    plus new class "Subscriber"

--- main.cs
+++ main.cs
@@ -4,14 +4,20 @@ using System.Linq;
 
 public class Program
 {
+  public  class Subscriber
+  {
+      public uint id = 0;
+      public string names = "";
+      public string books = "";
+  }
   public static void Main()
   {
-    Console.WriteLine("asda");
-    int[] id = new int [10];
+    
+    uint[] id = new uint [10];
     string[] names = new string[10] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
     string[] books = new string[10] {"A_book", "B_book", "C_book", "D_book", "E_book", "F_book", "G_book", "H_book", "I_book", "J_book"};
 
-    for (int i = 0; i < 10; i++)
+    for (uint i = 0; i < 10; i++)
     {
         id[i] = i;
     }
@@ -20,6 +26,12 @@ public class Program
     {
         Console.WriteLine($"id: {id[i]}\t Name: {names[i]}\t Book: {books[i]}");
     }
+
+    Subscriber newSubscriber = new Subscriber();
+    newSubscriber.id = 101;
+    newSubscriber.names = "Mark";
+    newSubscriber.books = "Mark_book";
+    Console.WriteLine($"ID: {newSubscriber.id}\t Names: {newSubscriber.names} \t Book: {newSubscriber.books}");
   }
 
 }
